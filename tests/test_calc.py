64b24b0c502560fc34d86676faf2b16a41aeb8ab
vladislaw-shkliarenko <90755976+vladislaw-shkliarenko@users.noreply.github.com>
2023-12-16 21:19:18 +0200

    Add files via upload

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -16,3 +16,35 @@ def test_subtract_positive():
 def test_subtract_negative():
     calc = Calculator(5, 10)
     assert calc.subtract() == -5
+
+def test_multiply_positive():
+    calc = Calculator(3, 7)
+    assert calc.multiply() == 21
+
+def test_multiply_by_zero():
+    calc = Calculator(5, 0)
+    assert calc.multiply() == 0
+    
+def test_divide_positive():
+    calc = Calculator(10, 2)
+    assert calc.divide() == 5
+
+def test_divide_by_zero():
+    calc = Calculator(10, 0)
+    assert calc.divide() == "Ділення на 0 неможливе"
+
+def test_power_positive():
+    calc = Calculator(2, 3)
+    assert calc.power() == 8
+
+def test_power_negative():
+    calc = Calculator(-2, 3)
+    assert calc.power() == -8
+
+def test_random_positive():
+    num = Calculator.random_number(1, 10)
+    assert 1 <= num <= 10
+
+def test_random_negative():
+    num = Calculator.random_number(10, 5)
+    assert num is None
